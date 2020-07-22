8bedda45e8cc3a1404e68d1a1e66c140d665f619
ssemenkoff <ssemenkoff@gmail.com>
2020-07-22 13:35:04 +0300

    Created pages

---
+++ pages/a.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="fullscreen a page">
+    <n-link class="button" to="/b">Go to b</n-link>
+  </div>
+</template>
+<style>
+  .a {
+    background: linear-gradient(90deg, rgba(18,13,120,1) 0%, rgba(47,36,233,1) 100%);
+  }
+</style>
---
+++ pages/b.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fullscreen b page">
+    <n-link class="button" to="/a">Go to a</n-link>
+  </div>
+</template>
+<style>
+  .page {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .b {
+    background: linear-gradient(90deg, rgba(103,14,14,1) 0%, rgba(199,31,31,1) 100%);
+  }
+
+  .button {
+    display: block;
+    padding: 10px 20px;
+    background-color: white;
+    border: 1px solid silver;
+    border-radius: 9999px;
+    color: black;
+    text-decoration: none;
+  }
+
+  .button:focus {
+    outline: none;
+    user-select: none;
+  }
+</style>
